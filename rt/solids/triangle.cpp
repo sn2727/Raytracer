de06ccf7ec0fcef63d95148b84f9aa1b08e3163e
Simon Rauch <s8snrauc@stud.uni-saarland.de>
2020-11-25 14:38:44 +0100

    triangle optimization

--- rt/solids/triangle.cpp
+++ rt/solids/triangle.cpp
@@ -14,51 +14,34 @@ Triangle::Triangle(const Point& v1, const Point& v2, const Point& v3, CoordMappe
     this -> v1 = v1;
     this -> v2 = v2;
     this -> v3 = v3;
-    Vector v1v2 = v2 - v1; 
-    Vector v1v3 = v3 - v1; 
-    this -> normal = cross(v1v2,v1v3);
+    this -> edge1 = v2 - v1; 
+    this -> edge2 = v3 - v1; 
+    this -> normal = cross(edge1,edge2);
     this -> area = 0.5f * normal.length();
-    this -> d = dot(normal, Vector(v1.x,v1.y,v1.z));
 }
 
 BBox Triangle::getBounds() const {
     /* TODO */ NOT_IMPLEMENTED;
 }
 
+//Moeller-Trumbore algorithm
 Intersection Triangle::intersect(const Ray& ray, float previousBestDistance) const {
        
-    float NdotRayDirection = dot(normal, ray.d); 
-    if (fabs(NdotRayDirection) < epsilon)
-        return Intersection::failure(); // parallel so they don't intersect  
- 
-    float t = (dot(normal, Vector(ray.o.x,ray.o.y,ray.o.z)) + d) / NdotRayDirection; 
+    Vector h = cross(ray.d, edge2);
+    float a = dot(edge1, h);
+    if (a > -epsilon && a < epsilon) return Intersection::failure(); //ray is parallel
+    float f = 1.0f/a;
+    Vector s = ray.o - v1;
+    float u = f*dot(s,h);
+    if (u < 0.0 || u > 1.0f) return Intersection::failure();
+    Vector q = cross(s, edge1);
+    float v = f*dot(ray.d, q);
+    if (v < 0.0 || u+v > 1.0f) return Intersection::failure();
+    float t = f*dot(edge2, q);
+    if (t > epsilon) {
+        return Intersection(t, ray, this, normal, ray.getPoint(t));
+    } else return Intersection::failure();
 
-    if (t < epsilon || previousBestDistance <= t) return Intersection::failure(); // the triangle is behind 
-    
-    Point hit = ray.getPoint(t);
-    Vector P = Vector(hit.x, hit.y, hit.z); 
- 
-    Vector C; // vector perpendicular to triangle's plane 
- 
-    // edge 1
-    Vector edge1 = v2 - v1; 
-    Vector vp1 = P - Vector(v1.x, v1.y, v1.z); 
-    C = cross(edge1, vp1); 
-    if (dot(normal, C) < 0) return Intersection::failure(); // P is on the right side 
- 
-    // edge 2
-    Vector edge2 = v3 - v2; 
-    Vector vp2 = P - Vector(v2.x,v2.y,v2.z); 
-    C = cross(edge2, vp2); 
-    if (dot(normal, C) < 0)  return Intersection::failure(); // P is on the right side 
- 
-    // edge 3
-    Vector edge3 = v1 - v3; 
-    Vector vp3 = P - Vector(v3.x,v3.y,v3.z); 
-    C = cross(edge3, vp3); 
-    if (dot(normal, C) < 0) return Intersection::failure(); // P is on the right side; 
- 
-    return Intersection(t, ray, this, normal, hit); 
 }
 
 
