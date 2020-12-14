23cbc5d3ef6d5ca13a67def4f643cd4c4eea745a
Simon <rauchsimon@protonmail.com>
2020-12-14 12:10:18 +0100

    spotlight fix

--- rt/lights/spotlight.cpp
+++ rt/lights/spotlight.cpp
@@ -1,21 +1,23 @@
 #include <rt/lights/spotlight.h>
+#include <iostream>
 
 namespace rt {
 
 SpotLight::SpotLight(const Point& position, const Vector& direction, float angle, float power, const RGBColor& intensity)
 {
     this -> position = position;
-    this -> direction = direction;
+    this -> direction = -direction;
     this -> angle = angle;
     this -> power = power;
     this -> intensity = intensity;
 }
 
 RGBColor SpotLight::getIntensity(const LightHit& irr) const {
-    if (dot(direction, irr.direction) >= angle) {
+    float ang = dot(direction, irr.direction);
+    if (ang >= angle) {
         return RGBColor::rep(0);
     }
-    return intensity * power;
+    return intensity/(irr.distance*irr.distance);
 }
 
 }
