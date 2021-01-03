c858ddbcbce8a26da1c42a6f52d3b7a32581aafd
Simon <rauchsimon@protonmail.com>
2021-01-03 12:10:18 +0100

    plane mapper

--- rt/coordmappers/plane.cpp
+++ rt/coordmappers/plane.cpp
@@ -3,15 +3,14 @@
 namespace rt {
 
 PlaneCoordMapper::PlaneCoordMapper(const Vector& e1, const Vector& e2) {
-    this -> e1 = e1.normalize();
-    this -> e2 = e2.normalize();
+    this -> e1 = e1;
+    this -> e2 = e2;
 }
 
 Point PlaneCoordMapper::getCoords(const Intersection& hit) const {
     Point hitP(hit.local());
-    float u = dot(e1, Vector(hitP.x, hitP.y, hitP.z));
-    float v = dot(e1, Vector(hitP.x, hitP.y, hitP.z));
-    return Point(u, v, e1.z);
+    Vector v(hitP.x*e1 + hitP.y*e2);
+    return Point(v.x, v.y, v.z);
 }
 
 }
