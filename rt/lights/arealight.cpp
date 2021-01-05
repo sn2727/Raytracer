5979e19e5f2ee6a35c5f106723735c45bf10f0c4
Simon <rauchsimon@protonmail.com>
2021-01-05 17:33:26 +0100

    arealight fixed

--- rt/lights/arealight.cpp
+++ rt/lights/arealight.cpp
@@ -5,16 +5,23 @@
 namespace rt {
 
 LightHit AreaLight::getLightHit(const Point& p) const {
-    return {Vector(1,0,0), 0, Vector(1,0,0)};
+    Vector dir = source->sample().point - p;
+    float distance = dir.length();
+    if (fabs(distance) < epsilon) {
+        return LightHit{dir, distance, Vector(0,0,0)};
+    }
+    return {dir, distance, dir.normalize()};
 }
 
 RGBColor AreaLight::getIntensity(const LightHit& irr) const {
     RGBColor color = RGBColor::rep(0);
-    for (int i = 0; i<SAMPLES; i++) {
+    Solid::Sample s = source->sample();
+    return source -> material -> getEmission(s.point, s.normal, Vector(1,0,0));
+    /*for (int i = 0; i<SAMPLES; i++) {
         Solid::Sample s = source->sample();
-        color = color + source->material->getEmission(s.point, Vector(1,0,0), Vector(1,0,0));
+        color = color + source->material->getEmission(s.point, s.normal, Vector(1,0,0));
     }
-    return color/SAMPLES;
+    return color/SAMPLES;*/
 }
 
 AreaLight::AreaLight(Solid* source)
